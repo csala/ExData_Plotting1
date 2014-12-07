0ed4d2029a7becb4780f63963fe04b11d9742051
csala <c.sala.stq@gmail.com>
2014-12-08 00:02:45 +0100

    Fix a couple of bugs and add png files

--- plot3.R
+++ plot3.R
@@ -6,7 +6,7 @@ grep_csv <- function(file, expr,...){
     data
 }
 load_data <- function(){
-    if file.exists('household_power_consumption.txt'){
+    if (file.exists('household_power_consumption.txt')){
         grep_csv('household_power_consumption.txt','^(1/2/2007|2/2/2007)',sep=';',stringsAsFactors=FALSE)
     } else {
         stop('File household_power_consumption.txt not found. Please download it from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip, uncompress it and try again')
--- plot4.R
+++ plot4.R
@@ -6,7 +6,7 @@ grep_csv <- function(file, expr,...){
     data
 }
 load_data <- function(){
-    if file.exists('household_power_consumption.txt'){
+    if (file.exists('household_power_consumption.txt')){
         grep_csv('household_power_consumption.txt','^(1/2/2007|2/2/2007)',sep=';',stringsAsFactors=FALSE)
     } else {
         stop('File household_power_consumption.txt not found. Please download it from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip, uncompress it and try again')
@@ -24,7 +24,7 @@ plot4 <- function(){
     par(mfrow=c(2,2))
 
     # top left
-    plot(data$DateTime, data$Global_active_power, type='line',ylab='Global Active Power (kilowatts)',xlab='')
+    plot(data$DateTime, data$Global_active_power, type='line',ylab='Global Active Power',xlab='')
 
     # top right
     plot(data$DateTime, data$Voltage, type='line',ylab='Voltage',xlab='datetime')
